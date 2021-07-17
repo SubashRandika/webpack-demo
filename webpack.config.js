5c96926a4dec4c3b24e437f9a4a5e7a315d27928
Randika Rodrigo <subash.rodrigo@gmail.com>
2021-07-17 23:42:21 +0530

    Implement cache busting with html-webpack-plugin.

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -5,9 +6,14 @@ module.exports = {
 	// devtool: false, // This option controls if and how source maps are generated.
 	entry: './src/index.js',
 	output: {
-		filename: 'main.js',
+		filename: 'main.[contenthash].js',
 		path: path.resolve(__dirname, 'dist')
 	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/template.html'
+		})
+	],
 	module: {
 		rules: [
 			{
