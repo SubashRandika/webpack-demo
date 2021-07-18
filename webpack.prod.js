0ecaa74475a6685356fa2481405b229f42fe99a0
Randika Rodrigo <subash.rodrigo@gmail.com>
2021-07-18 13:43:55 +0530

    Extracted CSS into seperate files using mini-css-extract-plugin webpack plugin for production.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,4 +1,5 @@
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
@@ -9,5 +10,21 @@ module.exports = merge(common, {
 		filename: '[name].[contenthash].bundle.js',
 		path: path.resolve(__dirname, 'dist')
 	},
-	plugins: [new CleanWebpackPlugin()]
+	plugins: [
+		new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }),
+		new CleanWebpackPlugin()
+	],
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: [
+					MiniCssExtractPlugin.loader, // 3. Extract CSS into seperate files
+					// 'style-loader', // 3. Inject styles into DOM
+					'css-loader', // 2. Turns css into commonjs
+					'sass-loader' // 1. Turns sass into css
+				]
+			}
+		]
+	}
 });
